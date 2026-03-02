0c9af5a7a3a261e87071f4de4a3203d1b6dd2d76
Kingsley <kingsleylyh0918@gmail.com>
2026-03-02 17:23:10 +0800

    Update

--- SYBAR/firebaseConfig.ts
+++ SYBAR/firebaseConfig.ts
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCa5Nz0z1GxwdULH2evUWg6yPSoWCCjVxY",
-  authDomain: "sybar-4cc79.firebaseapp.com",
-  projectId: "sybar-4cc79",
-  storageBucket: "sybar-4cc79.firebasestorage.app",
-  messagingSenderId: "771152439044",
-  appId: "1:771152439044:web:59fc34e0b656d3287dd251",
-  measurementId: "G-48H28Y1R2P"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
